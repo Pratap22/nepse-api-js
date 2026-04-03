4c556021c7dd679acbb1f100ec6e7a9ab50d042a
Pratap Sharma <sharma.pratap22@gmail.com>
2026-04-03 09:50:25 +0545

    Export default NEPSE Lib

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-export { Nepse } from "./nepseLib.js";
+import { Nepse } from "./nepseLib.js";
 export {
   NepseInvalidClientRequest,
   NepseInvalidServerResponse,
   NepseNetworkError,
   NepseTokenExpired
 } from "./errors.js";
+export { Nepse };
+export default Nepse;
